cd286180119964a6dff6547e2d381c20d9724481
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-09-22 17:12:35 +0900

    commit

--- 10830.py
+++ 10830.py
@@ -8,6 +8,11 @@ orig_matrix = []
 for i in range(n):
     orig_matrix.append(list(map(int, input().split())))
 
+for i in range(n):
+    for j in range(n):
+        if orig_matrix[i][j] >= 1000:
+            orig_matrix[i][j] = orig_matrix[i][j] % 1000
+
 
 def multiply(n, matrixA, matrixB):
     new_matrix = [[0 for i in range(n)] for j in range(n)]
@@ -22,10 +27,11 @@ def multiply(n, matrixA, matrixB):
 
 def divide(matrix, n, b):
     if b > 2:
+        temp = divide(matrix, n, b // 2)
         if b % 2 == 0:
-            return multiply(n, divide(matrix, n, b // 2), divide(matrix, n, b // 2))
+            return multiply(n, temp, temp)
         else:
-            return multiply(n, multiply(n, divide(matrix, n, b // 2), divide(matrix, n, b // 2)), matrix)
+            return multiply(n, multiply(n, temp, temp), matrix)
 
     elif b == 2:
         return multiply(n, matrix, matrix)
