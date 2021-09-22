b586c1d3584ba92b5037745e1f08794fb07250ad
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-09-22 20:36:16 +0900

    commit

--- 2579.py
+++ 2579.py
@@ -1,19 +1,19 @@
 # 2579 계단 오르기
+
 n = int(input())
 dp_table = []
 for i in range(n):
     dp_table.append(list(map(int, input().split())))
 
-for i in range(n):
-    for j in range(len(dp_table[n])):
-        if i != 0:
-            if j == 0:
-                dp_table[n][j] += dp_table[n - 1][j]
-            elif j == len(dp_table[n]) - 1:
-                dp_table[n][j] += dp_table[n - 1][j - 1]
-            elif dp_table[n - 1][j - 1] > dp_table[n - 1][j]:
-                dp_table[n][j] += dp_table[n - 1][j - 1]
-            else:
-                dp_table[n][j] += dp_table[n - 1][j]
+for line in range(1, n):
+    for j in range(len(dp_table[line])):
+        if j == 0:
+            dp_table[line][j] += dp_table[line - 1][j]
+        elif j == len(dp_table[line]) - 1:
+            dp_table[line][j] += dp_table[line - 1][j - 1]
+        elif dp_table[line - 1][j - 1] > dp_table[line - 1][j]:
+            dp_table[line][j] += dp_table[line - 1][j - 1]
+        else:
+            dp_table[line][j] += dp_table[line - 1][j]
 
-print(dp_table)
+print(max(dp_table[n - 1]))
