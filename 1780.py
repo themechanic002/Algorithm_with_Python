f4476bd014166ab057ff77934306f551805d5018
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-09-22 16:03:59 +0900

    commit

--- 1780.py
+++ 1780.py
@@ -24,17 +24,9 @@ def divide(x, y, len):
 
     isAllSameValue = isAllSame(x, y, len)
     if isAllSameValue == 2:
-        if len != 1:
-            for i in range(3):
-                for j in range(3):
-                    divide(x + (i * (len // 3)), y + (j * (len // 3)), len // 3)
-        else:
-            if paper[x][y] == -1:
-                minusone += 1
-            elif paper[x][y] == 0:
-                zero += 1
-            else:
-                plusone += 1
+        for i in range(3):
+            for j in range(3):
+                divide(x + (i * (len // 3)), y + (j * (len // 3)), len // 3)
     else:
         if isAllSameValue == -1:
             minusone += 1
