0e44880612165fe4ccd921e1eb62e4b549f391d8
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-09-27 11:07:26 +0900

    commit

--- 9184.py
+++ 9184.py
@@ -1,15 +1,35 @@
-a, b, c = map(int, input().split())
+# 9184 신나는 함수 실행
+
+d = [[[0 for col in range(21)] for row in range(21)] for depth in range(21)]
+
+while True:
+
+    a, b, c = map(int, input().split())
+    if a == -1 and b == -1 and c == -1:
+        break
 
 
-def w(a, b, c):
-    if a <= 0 or b <= 0 or c <= 0:
-        return 1
+    def w(a, b, c):
 
-    if a > 20 or b > 20 or c > 20:
-        return w(20, 20, 20)
+        if a <= 0 or b <= 0 or c <= 0:
+            return 1
 
-    if a < b and b < c:
-        return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        if a > 20 or b > 20 or c > 20:
+            return w(20, 20, 20)
 
-    else:
-        return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+        if a < b < c:
+            if d[a][b][c] == 0:
+                d[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+                return d[a][b][c]
+            else:
+                return d[a][b][c]
+
+        else:
+            if d[a][b][c] == 0:
+                d[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+                return d[a][b][c]
+            else:
+                return d[a][b][c]
+
+
+    print("w(" + str(a) + ", " + str(b) + ", " + str(c) + ") = " + str(w(a, b, c)))
