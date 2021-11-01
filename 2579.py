142598a5d42c84ff39558e2bc3602ccb759f8fc4
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-11-01 11:58:47 +0900

    commit

--- 2579.py
+++ 2579.py
@@ -11,9 +11,10 @@ for i in range(1, n + 1):
         d[i] = score[i]
     elif i == 2:
         d[i] = score[i] + score[i - 1]
-    elif i == 3:
-        d[i] = max(score[i] + score[i - 2], score[i] + score[i - 1])
     else:
         d[i] = max(d[i - 3] + score[i - 1] + score[i], d[i - 2] + score[i])
 
 print(d[n])
+
+# 3인 경우는 어차피 d[0] = 0 이니까 굳이 따로 elif 문으로 안해줘도 됨.
+# 황상 이렇게 점화식을 먼저 세우는 게 중요함.
